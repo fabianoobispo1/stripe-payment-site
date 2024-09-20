0e6956e6635dc3a7dbe7b83327a139ba69e12910
Your Name <fabianoobispo@icloud.com>
2024-09-20 13:28:53 -0300

    teste

--- components/planohmg/copyToClipboard.ts
+++
@@ -1,8 +0,0 @@
-export default function copyToClipboard(value: string): void {
-  const input = document.createElement('input');
-  input.value = value;
-  document.body.appendChild(input);
-  input.select();
-  document.execCommand('copy');
-  document.body.removeChild(input);
-}
